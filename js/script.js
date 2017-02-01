651b14b5e9feb2aa045aa98d770fbfcc63736709
Adam Orlowski <adamorlowskipoland@icloud.com>
2017-02-01 14:58:32 +0100

    I have written JS code again today, this time without looking at the solution

--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
-
-    
 function makeSound(e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
