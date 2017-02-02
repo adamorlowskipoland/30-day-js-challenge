fef4be0a8dd79e7f12184d233ba08634c8576763
Adam Orlowski <adamorlowskipoland@icloud.com>
2017-02-02 17:27:37 +0100

    updated testsiebie js file with digital clock

--- testsiebie.js
+++ testsiebie.js
@@ -15,6 +15,29 @@ function workClock() {
     secHand.style.transform = `rotate(${secDeg}deg)`;
     minHand.style.transform = `rotate(${minDeg}deg)`;
     hourHand.style.transform = `rotate(${hourDeg}deg)`;
+    
+    
+    
+    function digitalClock(a, b, c) {
+        if (a < 10) {
+            a = "0" + a;
+        }
+        if (b < 10) {
+            b = "0" + b;
+        }
+        if (c < 10) {
+            c = "0" + c;
+        }
+        
+        const hms = a + ":" + b + ":" + c;
+        
+        const date = document.querySelector(".date");
+        date.innerHTML = hms;
+    }
+    
+    digitalClock(hour, min, sec);
+    
+
 }
 
 setInterval(workClock, 1000);
