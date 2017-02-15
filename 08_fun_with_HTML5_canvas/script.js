a105ded1bfa8f4589a6167b8d7bb26ab24aac54f
Adam Orlowski <adamorlowskipoland@icloud.com>
2017-02-15 16:18:52 +0100

    add changing colors and width

--- 08_fun_with_HTML5_canvas/script.js
+++ 08_fun_with_HTML5_canvas/script.js
@@ -6,19 +6,36 @@ ctx.strokeStyle = '#BADA55';
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
 ctx.lineWidth = 20;
+//ctx.globalCompositeOperation = 'multiply';
+//look up for this on the internet
 
 let isDrawing = false;
 let lastX = 0;
 let lastY = 0;
+//using mother-effing: http://mothereffinghsl.com/
+let hue = 0;
+let direction = true;
 
 function draw(e) {
     if(!isDrawing) return;
     console.log(e);
+    ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
     ctx.beginPath();
     ctx.moveTo(lastX, lastY);
     ctx.lineTo(e.offsetX, e.offsetY);
     ctx.stroke();
     [lastX, lastY] = [e.offsetX, e.offsetY];
+    hue++;
+    if (hue === 360) {hue = 0;}
+    
+    if (ctx.lineWidth === 100 || ctx.lineWidth === 10) {
+        direction = !direction;
+    }
+    if (direction) {
+        ctx.lineWidth++;
+    } else {
+        ctx.lineWidth--;
+    }
 }
 
 canvas.addEventListener('mousedown', (e) => {
