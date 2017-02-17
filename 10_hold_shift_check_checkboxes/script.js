79066bf59fd69938ec55fc5693273f3c99b47dc0
Adam Orlowski <adamorlowskipoland@icloud.com>
2017-02-17 17:59:20 +0100

    you can now check many checkboxes holding down a shiftKey

--- 10_hold_shift_check_checkboxes/script.js
+++ 10_hold_shift_check_checkboxes/script.js
@@ -6,13 +6,22 @@ let lastChecked;
 function handleCheck(e) {
     // 1. check if the shift key was down
     // 2. check if the check is being checked not uncheking
+    let inBetween = false;
     if (e.shiftKey && this.checked) {
-        
+        // loop over every single checkbox
+        checkBoxes.forEach(checkBox => {
+            console.log(checkBox);
+            if (checkBox === this || checkBox === lastChecked) {
+                inBetween = !inBetween;
+            }
+            if (inBetween) {
+                checkBox.checked = true;
+            }
+        });
     }
     
     
     lastChecked = this;
-    console.log(lastChecked);
 }
 
 checkBoxes.forEach(checkBox => checkBox.addEventListener('click', handleCheck));
