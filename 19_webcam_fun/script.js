43cc01349e61bb2f98f011dfdb26c047b167777e
Adam Orlowski <adamorlowskipoland@icloud.com>
2017-02-23 17:26:32 +0100

    add localhost with package.json

--- 19_webcam_fun/script.js
+++ 19_webcam_fun/script.js
@@ -4,3 +4,23 @@ const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
 
+function getVideo() {
+    navigator.mediaDevices.getUserMedia({ video: true, audio: false }).then(localMediaStream => {
+        console.log(localMediaStream);
+        video.src = window.URL.createObjectURL(localMediaStream);
+        video.play();
+    })
+    
+    .catch(err => {
+        console.log(`This won't work without access to you camera`, err);
+    })
+}
+
+function paintToCanvas() {
+    const width = video.videoWidth;
+    const height = video.videoHeight;
+    console.log(width, height);
+}
+
+getVideo();
+paintToCanvas();
