c938a15cd49dc612bd053ff703b504a0c6671a49
Adam Orlowski <adamorlowskipoland@icloud.com>
2017-03-13 13:37:47 +0100

    buttons and input works

--- 29_countdown_timer/script.js
+++ 29_countdown_timer/script.js
@@ -1,8 +1,11 @@
+let countDown;
 const timerDisplay = document.querySelector('.display__time-left');
 const endTime = document.querySelector('.display__end-time');
+const buttons = document.querySelectorAll('[data-time]');
 
 function timer (seconds) {
-    let countDown;
+    // first you need to clear any existing timers
+    clearInterval(countDown);
     const now = Date.now();
     const then = now + seconds * 1000;
     displayTimeLeft(seconds);
@@ -26,14 +29,36 @@ function displayTimeLeft (seconds) {
     const display = `${minutes}:${remainderSeconds < 10 ? "0" : ""}${remainderSeconds}`;
     timerDisplay.textContent = display;
     document.title = display;
-    console.log({minutes, remainderSeconds});
+//    console.log({minutes, remainderSeconds});
 }
 
 function displayEndTime (timestamp) {
     const end = new Date(timestamp);
     const hour = end.getHours();
     // line for american time display
-    const adjustedHour = hour > 12 ? hour - 12 : hour;
+    // put adjustedHour in endTime.textContent
+//    const adjustedHour = hour > 12 ? hour - 12 : hour;
     const minutes = end.getMinutes();
     endTime.textContent = `Be back at ${hour}:${minutes < 10 ? "0" : ""}${minutes}`; 
 }
+
+function startTimer () {
+//    console.log(this);
+//    console.log(this.dataset);
+    
+    // this is what we need
+//    console.log(this.dataset.time);
+    
+    const seconds = parseInt(this.dataset.time);
+    timer(seconds);
+//    console.log(seconds);
+}
+
+buttons.forEach(button => button.addEventListener('click', startTimer));
+document.customForm.addEventListener('submit', function(e) {
+    e.preventDefault();
+    const mins = this.minutes.value;
+//    console.log(mins);
+    timer(mins * 60);
+    this.reset();
+});
