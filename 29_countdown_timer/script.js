a7e7a3ce2a60e385e22cbbdf56c5bb604bd411e1
Adam Orlowski <adamorlowskipoland@icloud.com>
2017-03-13 12:56:22 +0100

    console loging minutes and seconds left

--- 29_countdown_timer/script.js
+++ 29_countdown_timer/script.js
@@ -2,17 +2,22 @@ function timer (seconds) {
     let countDown;
     const now = Date.now();
     const then = now + seconds * 1000;
+    displayTimeLeft(seconds);
     
     countDown = setInterval(() => {
         const secondsLeft = Math.round((then - Date.now()) / 1000);
         // check if we should stop
-        if (secondsLeft <= 0) {
+        if (secondsLeft < 0) {
             clearInterval(countDown);
             return;
         }
         // display 
-        console.log(secondsLeft);
+        displayTimeLeft(secondsLeft);
     }, 1000);
-    
-    console.log(now, then);
+}
+
+function displayTimeLeft (seconds) {
+    const minutes = Math.floor(seconds / 60);
+    const remainderSeconds = seconds % 60;
+    console.log({minutes, remainderSeconds});
 }
